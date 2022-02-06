4c751aa718547cf0ab1b92bf1466c917be0c8c41
Riyanna2233 <riyanna2233@gmail.com>
2022-02-06 19:38:23 +0530

    database

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('./models/toDoModel')
 const toDoModel = mongoose.model('todo')
+require('dotenv').config()
 
-mongoose.connect('mongodb://127.0.0.1:27017/todo',{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DATABASE_URI,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>console.log("The db is connected"))
 .catch((err)=>console.log(err))
 
